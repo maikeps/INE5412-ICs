f6cb3c64a8bf84ed60a66d4e4dbbf0c993c3d240
Maikeps <psmaike@gmail.com>
2016-03-28 19:07:40 -0300

    IC2 finalizado.

--- IC2/main.cpp
+++ IC2/main.cpp
@@ -8,7 +8,7 @@
 #include <sys/wait.h>
 
 #define SHARED_OBJ_NAME     "/oioioi1234"
-#define MAX_MSG_LENGTH      /* max length */
+#define MAX_MSG_LENGTH      64 /* max length */
 
 struct message_struct {
     pid_t sender;
@@ -22,57 +22,80 @@ int main() {
     int shared_seg_size = sizeof(struct message_struct);   
     struct message_struct *shared_message;  
 
-    cout << "Parent process " << /* process ID */ << " is creating a child process" << endl;
+    cout << "Parent process " << getpid() << " is creating a child process" << endl;
     
     /* duplicate process */
+    pid_t pid = fork();
     
     /* if error in duplicating */
+    if(pid < 0){
         return -1;
+    }
         
     /* if child-process running then */
-        cout << "Child process " << /* process ID */ << " is creating a shared memory object to write a message in" << endl;
-        shared_object = /* create shared object */
+    if(pid == 0){
+        cout << "Child process " << getpid() << " is creating a shared memory object to write a message in" << endl;
+
+        /* create shared object */
+        shared_object = shm_open(SHARED_OBJ_NAME, O_CREAT | O_EXCL | O_RDWR, S_IRWXU);
         if (shared_object < 0) {
             cout << "Error "<< shared_object << " creating shared object" << endl;
             return -1;
         }
+
         /*  make room for the shared object to fit a message*/
-        //...
-        shared_message = (struct message_struct *) /* map the shared onject to memory */
-        if (shared_message == NULL) {
-            cout << "Error in memory map" << endl;
-            return -1;
-        }
-        /* producing a message on the shared segment */
-        shared_message->type = 1;
-	    shared_message->sender = /* process ID */
-	    strcpy(shared_message->content, /* message to be sent */);
-        cout << "Child process " << /* process ID */ << " wrote message '" << shared_message->content << "' in memory" << endl;
-        return 0;
-      
-    /* if parent-process running then */
-        cout << "Parent process " << /* process ID */ << " is waiting for child to exit" << endl;
-        /* wait for child process to exit and get its status */
-	    /* if status is not success */
-	        cout << "Parent process " << /* process ID */ << " is exiting since child could not write message in memory" << endl;
-	        return -1;
-	
-        cout << "Parent process " << /* process ID */ << " will read message from process " << /* child process ID */ << " finished with status "<< /* status of finished child process */ << endl;
-        shared_object = /* create the shared object to read from */
-        if (shared_object < 0) {
-            cout << "Error in shm_open()" << endl;
-            return -1;
-        }
-            
-        shared_message = (struct message_struct *)/* map the shared object to memory */
+        ftruncate(shared_object, shared_seg_size);
+
+        /* map the shared onject to memory */
+        shared_message = (struct message_struct *) mmap(NULL, shared_seg_size, PROT_READ | PROT_WRITE, MAP_SHARED, shared_object, 0);
+
         if (shared_message == NULL) {
             cout << "Error in memory map" << endl;
             return -1;
         }
 
-        cout << "Parent process " << /* process ID */ << " read the message '" << shared_message->content << "' from sender " << shared_message->sender << " in memory " << endl;
+        /* producing a message on the shared segment */
+        shared_message->type = 1;
+	    shared_message->sender = getpid();
+	    strcpy(shared_message->content, "Message (:");
+
+        cout << "Child process " << getpid() << " wrote message '" << shared_message->content << "' in memory" << endl;
+        return 0;
+    }
+     
+    /* if parent-process running then */
+    if(pid > 0){
+        cout << "Parent process " << getpid() << " is waiting for child to exit" << endl;
+        
+        int status, err = 0;
+        /* wait for child process to exit and get its status */
+        err = waitpid(0, &status, 0);
+
+	    /* if status is not success */
+        int exit_status = WEXITSTATUS(status);
+        if(exit_status < 0){
+	        cout << "Parent process " << getpid() << " is exiting since child could not write message in memory" << endl;
+	        return -1;
+	    }
+
+        cout << "Parent process " << getpid() << " will read message from process " << pid << " finished with status "<< exit_status << endl;
+
+        // Flag S_IRWXU: Read, write, and execute by owner.
+        shared_object = shm_open(SHARED_OBJ_NAME, O_RDWR, S_IRWXU);
+        if (shared_object < 0) {
+            cout << "Error in shm_open()" << endl;
+            return -1;
+        }
+            
+        shared_message = (struct message_struct *) mmap(NULL, shared_seg_size, PROT_READ | PROT_WRITE, MAP_SHARED, shared_object, 0);
+        if (shared_message == NULL) {
+            cout << "Error in memory map" << endl;
+            return -1;
+        }
+
+        cout << "Parent process " << getpid() << " read the message '" << shared_message->content << "' from sender " << shared_message->sender << " in memory " << endl;
    
-        int removed = /* remove the shared object */
+        int removed = shm_unlink(SHARED_OBJ_NAME);
         if (removed != 0) {
             cout << "Error removing the shared object" << endl;
             return -1;
